1420364829974c608f73010a5e46b3a245c97fe1
Balaji <balajikaredla12345@gmail.com>
2024-03-18 23:59:25 +0530

    Unportunatelee Merge not sexesfull

--- main.py
+++ main.py
@@ -3,9 +3,5 @@ import script, aayush, new_script, scriptblue, scriptred, vishwa
 from sample_scripts import sample1, sample2, sample3
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     G = Game((40, 40), vishwa, aayush)
-=======
-    G = Game((40, 40), script, scriptred)
->>>>>>> a07d69eda85ff7ee1ccbc11eeb1f197483af30ff
     G.run_game()
