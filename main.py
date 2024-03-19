45b0abe9b84a2effb2d89e35cb273d6a16434fe8
“AdityaNeeraje” <aditya.neeraje@gmail.com>
2024-03-19 19:47:48 +0530

    Hello

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import script, aayush, new_script, scriptblue, scriptred, vishwa
 from sample_scripts import sample1, sample2, sample3
 
 if __name__ == "__main__":
-    G = Game((32, 32), script, vishwa)
+    G = Game((32, 32), vishwa, script)
     G.run_game()
